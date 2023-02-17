817cb91748e5875e236b6a1c368d8b4830f8f99c
hyunjaekwon <hyunjaekwon@gmail.com>
2023-02-17 14:22:18 -0800

    refactor FSM-based measurement flow to a new MM subclass called MeasurementManagerFSM

--- src/bag/simulation/measure.py
+++ src/bag/simulation/measure.py
@@ -68,6 +68,135 @@ class MeasurementManager(LoggingBase, abc.ABC):
     def precision(self) -> int:
         return self._precision
 
+    def commit(self) -> None:
+        """Commit changes to specs dictionary.  Perform necessary initialization."""
+        pass
+
+    def make_tbm(self, tbm_cls: Union[Type[TestbenchManager], str], tbm_specs: Mapping[str, Any],
+                 ) -> TestbenchManager:
+        obj_cls = cast(Type[TestbenchManager], import_class(tbm_cls))
+        return obj_cls(None, Path(), '', '', tbm_specs, None, None,
+                       precision=self._precision, logger=self.logger)
+
+    def make_mm(self, mm_cls: Union[Type[MeasurementManager], str], mm_specs: Mapping[str, Any]
+                ) -> MeasurementManager:
+        obj_cls = cast(Type[MeasurementManager], import_class(mm_cls))
+        return obj_cls(mm_specs, self.log_file, log_level=self.log_level, precision=self._precision)
+
+    @abc.abstractmethod
+    async def async_measure_performance(self, name: str, sim_dir: Path, sim_db: SimulationDB,
+                                        dut: Optional[DesignInstance],
+                                        harnesses: Optional[Sequence[DesignInstance]] = None) -> Mapping[str, Any]:
+        """A coroutine that performs measurement.
+
+        Parameters
+        ----------
+        name : str
+            name of this measurement.
+        sim_dir : Path
+            simulation directory.
+        sim_db : SimulationDB
+            the simulation database object.
+        dut : Optional[DesignInstance]
+            the DUT to measure.
+        harnesses : Optional[Sequence[DesignInstance]]
+            the list of DUT and harnesses to measure.
+
+        Returns
+        -------
+        output : Mapping[str, Any]
+            the measurement results.
+        """
+        pass
+
+    def measure_performance(self, name: str, sim_dir: Path, sim_db: SimulationDB,
+                            dut: Optional[DesignInstance], harnesses: Optional[Sequence[DesignInstance]] = None
+                            ) -> Mapping[str, Any]:
+        if harnesses:
+            coro = self.async_measure_performance(name, sim_dir, sim_db, dut, harnesses)
+        else:  # for backwards compatibility
+            coro = self.async_measure_performance(name, sim_dir, sim_db, dut)
+        results = batch_async_task([coro])
+        if results is None:
+            return {}
+
+        ans = results[0]
+        if isinstance(ans, Exception):
+            raise ans
+        return ans
+
+
+class MeasurementManagerFSM(MeasurementManager, abc.ABC):
+    """A class that handles circuit performance measurement in an FSM-like fashion.
+
+    Any subclass of MeasurementManagerFSM will need to implement the following methods:
+        initialize, process_output, get_sim_info.
+
+    Refer to async_measure_performance to see how the above methods are integrated together into the measurement process.
+    """
+
+    async def async_measure_performance(self, name: str, sim_dir: Path, sim_db: SimulationDB,
+                                        dut: Optional[DesignInstance],
+                                        harnesses: Optional[Sequence[DesignInstance]] = None) -> Mapping[str, Any]:
+        """A coroutine that performs measurement.
+
+        The measurement is done like a FSM.  On each iteration, depending on the current
+        state, it creates a new testbench (or reuse an existing one) and simulate it.
+        It then post-process the simulation data to determine the next FSM state, or
+        if the measurement is done.
+
+        Parameters
+        ----------
+        name : str
+            name of this measurement.
+        sim_dir : Path
+            simulation directory.
+        sim_db : SimulationDB
+            the simulation database object.
+        dut : Optional[DesignInstance]
+            the DUT to measure.
+        harnesses : Optional[Sequence[DesignInstance]]
+            the list of DUT and harnesses to measure.
+
+        Returns
+        -------
+        output : Mapping[str, Any]
+            the last dictionary returned by process_output().
+        """
+        if harnesses:
+            done, cur_info = self.initialize(sim_db, dut, harnesses)
+        else:  # for backwards compatibility
+            done, cur_info = self.initialize(sim_db, dut)
+        while not done:
+            cur_state = cur_info.state
+            self.log(f'Measurement {name}, state {cur_state}')
+            sim_id = f'{name}_{cur_state}'
+
+            # create and setup testbench
+            if harnesses:
+                sim_object, use_dut = self.get_sim_info(sim_db, dut, cur_info, harnesses)
+            else:  # for backwards compatibility
+                sim_object, use_dut = self.get_sim_info(sim_db, dut, cur_info)
+            cur_dut = dut if use_dut else None
+            if isinstance(sim_object, MeasurementManager):
+                sim_results = await sim_db.async_simulate_mm_obj(sim_id, sim_dir / cur_state,
+                                                                 cur_dut, sim_object, harnesses)
+            else:
+                tbm, tb_params = sim_object
+                sim_results = await sim_db.async_simulate_tbm_obj(cur_state, sim_dir / cur_state,
+                                                                  cur_dut, tbm, tb_params,
+                                                                  tb_name=sim_id, harnesses=harnesses)
+
+            self.log(f'Processing output of {name}, state {cur_state}')
+            done, next_info = self.process_output(cur_info, sim_results)
+            write_yaml(sim_dir / f'{cur_state}.yaml', next_info.prev_results)
+            cur_info = next_info
+
+        self.log(f'Measurement {name} done, recording results.')
+        result = cur_info.prev_results
+        write_yaml(sim_dir / f'{name}.yaml', cur_info.prev_results)
+        return result
+
     @abc.abstractmethod
     def initialize(self, sim_db: SimulationDB, dut: DesignInstance,
                    harnesses: Optional[Sequence[DesignInstance]] = None) -> Tuple[bool, MeasInfo]:
@@ -140,96 +269,3 @@ class MeasurementManager(LoggingBase, abc.ABC):
             True to run simulation with DesignInstance.
         """
         pass
-
-    def commit(self) -> None:
-        """Commit changes to specs dictionary.  Perform necessary initialization."""
-        pass
-
-    def make_tbm(self, tbm_cls: Union[Type[TestbenchManager], str], tbm_specs: Mapping[str, Any],
-                 ) -> TestbenchManager:
-        obj_cls = cast(Type[TestbenchManager], import_class(tbm_cls))
-        return obj_cls(None, Path(), '', '', tbm_specs, None, None,
-                       precision=self._precision, logger=self.logger)
-
-    def make_mm(self, mm_cls: Union[Type[MeasurementManager], str], mm_specs: Mapping[str, Any]
-                ) -> MeasurementManager:
-        obj_cls = cast(Type[MeasurementManager], import_class(mm_cls))
-        return obj_cls(mm_specs, self.log_file, log_level=self.log_level, precision=self._precision)
-
-    async def async_measure_performance(self, name: str, sim_dir: Path, sim_db: SimulationDB,
-                                        dut: Optional[DesignInstance],
-                                        harnesses: Optional[Sequence[DesignInstance]] = None) -> Mapping[str, Any]:
-        """A coroutine that performs measurement.
-
-        The measurement is done like a FSM.  On each iteration, depending on the current
-        state, it creates a new testbench (or reuse an existing one) and simulate it.
-        It then post-process the simulation data to determine the next FSM state, or
-        if the measurement is done.
-
-        Parameters
-        ----------
-        name : str
-            name of this measurement.
-        sim_dir : Path
-            simulation directory.
-        sim_db : SimulationDB
-            the simulation database object.
-        dut : Optional[DesignInstance]
-            the DUT to measure.
-        harnesses : Optional[Sequence[DesignInstance]]
-            the list of DUT and harnesses to measure.
-
-        Returns
-        -------
-        output : Mapping[str, Any]
-            the last dictionary returned by process_output().
-        """
-        if harnesses:
-            done, cur_info = self.initialize(sim_db, dut, harnesses)
-        else:  # for backwards compatibility
-            done, cur_info = self.initialize(sim_db, dut)
-        while not done:
-            cur_state = cur_info.state
-            self.log(f'Measurement {name}, state {cur_state}')
-            sim_id = f'{name}_{cur_state}'
-
-            # create and setup testbench
-            if harnesses:
-                sim_object, use_dut = self.get_sim_info(sim_db, dut, cur_info, harnesses)
-            else:  # for backwards compatibility
-                sim_object, use_dut = self.get_sim_info(sim_db, dut, cur_info)
-            cur_dut = dut if use_dut else None
-            if isinstance(sim_object, MeasurementManager):
-                sim_results = await sim_db.async_simulate_mm_obj(sim_id, sim_dir / cur_state,
-                                                                 cur_dut, sim_object, harnesses)
-            else:
-                tbm, tb_params = sim_object
-                sim_results = await sim_db.async_simulate_tbm_obj(cur_state, sim_dir / cur_state,
-                                                                  cur_dut, tbm, tb_params,
-                                                                  tb_name=sim_id, harnesses=harnesses)
-
-            self.log(f'Processing output of {name}, state {cur_state}')
-            done, next_info = self.process_output(cur_info, sim_results)
-            write_yaml(sim_dir / f'{cur_state}.yaml', next_info.prev_results)
-            cur_info = next_info
-
-        self.log(f'Measurement {name} done, recording results.')
-        result = cur_info.prev_results
-        write_yaml(sim_dir / f'{name}.yaml', cur_info.prev_results)
-        return result
-
-    def measure_performance(self, name: str, sim_dir: Path, sim_db: SimulationDB,
-                            dut: Optional[DesignInstance], harnesses: Optional[Sequence[DesignInstance]] = None
-                            ) -> Mapping[str, Any]:
-        if harnesses:
-            coro = self.async_measure_performance(name, sim_dir, sim_db, dut, harnesses)
-        else:  # for backwards compatibility
-            coro = self.async_measure_performance(name, sim_dir, sim_db, dut)
-        results = batch_async_task([coro])
-        if results is None:
-            return {}
-
-        ans = results[0]
-        if isinstance(ans, Exception):
-            raise ans
-        return ans
